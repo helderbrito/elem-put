d89bf9a7b330d71377471742b3a7a9e78b5d9852
Helder Brito <helder.brito@outlook.com>
2024-08-31 22:24:06 -0300

    First version

---
+++ Models/ElemPutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElemPut.Models
+{
+    public class ElemPutModel
+    {
+        public bool Filter { get; set; }
+        public List<FormRow> Form { get; set; }
+        public List<Table> Table { get; set; }
+    }
+}
